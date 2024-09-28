91e69c0d33df016ab11eadf4c8652b71544fe1c7
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-09-28 15:46:59 +0000

    updated project settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,39 +29,33 @@ class Settings(BaseSettings):
     # ------------------------------------------------------------
     # project config definitions
     DOMAIN: str = "localhost"
-    PROJECT_NAME: str = ""
+    PROJECT_NAME: str = "UITS SYSLAB BACKEND"
     PROJECT_API_VERSION: str = "/api/v1"
     DOCKER_HOST_URL: str = "tcp://192.168.1.154:2376"
+
+    WEB_PORT_END: int = 9000
+    DNS_PORT_END: int = 6000
+    DNS_PORT_START: int = 5500
+    WEB_PORT_START: int = 8100
+
+    ADMIN_USER: str
+    ADMIN_PASSWORD: str
+
+    DOCKER_REPO_USER: str
+    DOCKER_REPO_URL: str
+    DEFAULT_UNIX_IMAGE: str
+    DOCKER_REPO_PASSWORD: str
 
     BACKEND_CORS_ORIGINS: Annotated[
         list[AnyUrl] | str, BeforeValidator(parse_cors)
     ] = ["http://localhost:3000", "http://localhost:8000", 'http://192.254.0.115:3000']
 
-    DB_URL: str
     # ------------------------------------------------------------
     # security config definitions
     JWT_ALGORITHM: str = "HS256"
     JWT_SECRET_KEY: str = secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 12  # 12 days
-
-    POSTGRES_SERVER: str
-    POSTGRES_PORT: int = 5432
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str = ""
-
-    @computed_field  # type: ignore[misc]
-    @property
-    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
-        )
 
     model_config = SettingsConfigDict(
         env_file='.env'
