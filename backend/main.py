ec035eaee5a1e9737af61bf62ad648c823347d2d
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-09-28 15:42:46 +0000

    added global exception handler

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,26 @@
+import logging
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
 
 from config.database import create_db_and_tables
 from config.settings import settings
 from routes.user import lab_router
 from routes.container import container_router
+from core.handlers import register_all_errors
 
-app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def custom_generate_unique_id(route: APIRoute) -> str:
+    return f"{route.tags[0]}-{route.name}"
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    generate_unique_id_function=custom_generate_unique_id,
+)
 app.include_router(lab_router)
 app.include_router(container_router)
 
@@ -22,8 +36,4 @@ if settings.BACKEND_CORS_ORIGINS:
         allow_headers=["*"],
     )
 
-
-@app.on_event("startup")
-def on_startup():
-    print("Creating DB Tables")
-    create_db_and_tables()
+register_all_errors(app)
